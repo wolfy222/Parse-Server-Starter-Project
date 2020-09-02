215fb997f7f6f841020f6d1e29334b8145fc2a97
Aadarsh Agarwal <aadarshagarwal2229@gmail.com>
2020-09-02 16:06:41 +0530

    Initial Commit

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -7,12 +7,18 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 package com.parse.starter;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.GetCallback;
@@ -30,237 +36,113 @@ import com.parse.SignUpCallback;
 import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
+
+  Boolean signUpModeActive = true;
+  TextView logInTextView;
+
+  @Override
+  public void onClick(View v)
+  {
+    if(v.getId() == R.id.logInTextView)
+    {
+//      Log.i("Switch"," was tapped!");
+      Button signUpButton = findViewById(R.id.signUpButton);
+
+      if(signUpModeActive)
+      {
+        signUpModeActive =false;
+        signUpButton.setText("Login");
+        logInTextView.setText("Sign Up, instead?");
+      }
+      else
+      {
+        signUpModeActive =true;
+        signUpButton.setText("Sign Up");
+        logInTextView.setText("Login in, insted? ");
+      }
+
+    }
+
+  }
+
+  public void signUpClicked(View view)
+  {
+
+    EditText usernameEditText= findViewById(R.id.usernameEditText);
+    EditText passwordEditText= findViewById(R.id.passwordEditText);
+    InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+    mgr.hideSoftInputFromWindow(passwordEditText.getWindowToken(), 0);
+
+    if (usernameEditText.getText().toString().equals("") || passwordEditText.getText().toString().equals(""))
+    {
+      Toast.makeText(this, "A username and a password are required", Toast.LENGTH_SHORT).show();
+
+    }
+    else
+    {
+      if (signUpModeActive)
+      {
+        // Sing up a user
+        ParseUser user = new ParseUser();
+        user.setUsername(usernameEditText.getText().toString());
+        user.setPassword(passwordEditText.getText().toString());
+
+        user.signUpInBackground(new SignUpCallback()
+        {
+          @Override
+          public void done(ParseException e)
+          {
+            if (e == null)
+            {
+              Log.i("Sign Up!", "Success");
+              Toast.makeText(MainActivity.this, "Successfully Signed Up!", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+              Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+          }
+        });
+      }
+      else
+      {
+        // LOGIN CODE
+
+        ParseUser.logInInBackground(usernameEditText.getText().toString(), passwordEditText.getText().toString(), new LogInCallback() {
+          @Override
+          public void done(ParseUser user, ParseException e)
+          {
+            if(user != null )
+            {
+              Log.i("Login","ok!");
+            }
+            else
+            {
+              Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+          }
+        });
+      }
+    }
+
+  }
 
 
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
+  protected void onCreate(Bundle savedInstanceState)
+  {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-/*
 
-
-    // //Adding Info in parse server
-
-    //Create Video Game to save score
-    //Class is like a table
-
-    ParseObject score = new ParseObject("Score");
-
-    //     Column Name  Below it
-    score.put("username","sean");
-    score.put("score",85);
-
-
-    //to save it in parse
-    score.saveInBackground(new SaveCallback() {
-      @Override
-      public void done(ParseException e)
-      {
-        if(e == null)
-        {
-          // OK
-          Log.i("Success","We saved the score");
-        }
-        else
-        {
-          e.printStackTrace();
-        }
-      }
-    });
-
- */
-
-/*
-
-    //To get info from parse server
-
-    ParseQuery<ParseObject> query = ParseQuery.getQuery("Score");
-    //Pass object id from parse server
-    query.getInBackground("Bj2338YD6x", new GetCallback<ParseObject>() {
-      @Override
-      public void done(ParseObject object, ParseException e)
-      {
-        if(e == null && object != null)
-        {
-          // OK
-          //Updating Info
-          object.put("score",85);
-          object.saveInBackground();
-
-          Log.i("Username",object.getString("username"));
-          Log.i("Score",Integer.toString(object.getInt("score")));
-
-        }
-        else
-        {
-          e.printStackTrace();
-        }
-
-
-      }
-    });
-
- */
-
-/*
-    // To grab all the objects
-
-    ParseQuery<ParseObject> query = ParseQuery.getQuery("Score");
-
-    // TO SEARCH FOR AN OBJECT
-    query.whereEqualTo("username","nick");
-    query.setLimit(1);
-
-    // to get everything
-    query.findInBackground(new FindCallback<ParseObject>() {
-      @Override
-      public void done(List<ParseObject> objects, ParseException e)
-      {
-        if(e == null )
-        {
-          if(objects.size() > 0)
-          {
-            for (ParseObject object : objects)
-            {
-              Log.i("Username", object.getString("username"));
-              Log.i("Score", Integer.toString(object.getInt("score")));
-            }
-          }
-        }
-        else
-        {
-          e.printStackTrace();
-        }
-      }
-    });
-
-
- */
-
-/*
-
-    //  TO GET A PERSON WHOSE SCORE IS GREATER THAN 50, GIVE THEM ADDITIONAL 20 POINTS
-
-
-    ParseQuery<ParseObject> query = ParseQuery.getQuery("Score");
-
-    // TO SEARCH FOR AN OBJECT
-    query.whereGreaterThan("score",50);
-
-    // to get everything
-    query.findInBackground(new FindCallback<ParseObject>() {
-      @Override
-      public void done(List<ParseObject> objects, ParseException e)
-      {
-        if(e == null )
-        {
-          if(objects.size() > 0)
-          {
-            for (ParseObject object : objects)
-            {
-              object.put("score",object.getInt("score")+20);
-              object.saveInBackground();
-              Log.i("Username", object.getString("username"));
-              Log.i("Score", Integer.toString(object.getInt("score")));
-            }
-          }
-        }
-        else
-        {
-          e.printStackTrace();
-        }
-      }
-    });
-
-*/
-
-
-/*
-
-    // SIGN UP CODE
-    //Creating a new User
-
-    ParseUser user = new ParseUser();
-    user.setUsername("nick");
-    user.setPassword("myPass");
-
-    // Sing up a user
-    user.signUpInBackground(new SignUpCallback() {
-      @Override
-      public void done(ParseException e)
-      {
-        if(e == null)
-        {
-          // OK
-          Log.i("Sign Up OK!","We did it");
-        }
-        else
-        {
-          e.printStackTrace();
-        }
-
-      }
-    });
-
-    */
-
-
-/*
-
-   // LOGIN CODE
-
-    ParseUser.logInInBackground("nick", "myPass", new LogInCallback() {
-      @Override
-      public void done(ParseUser user, ParseException e)
-      {
-        if(user != null )
-        {
-          Log.i("Success","We logged in");
-        }
-        else
-        {
-          e.printStackTrace();
-        }
-
-      }
-    });
-
-
- */
-
-/*
-
-   // CHECK IF THERE'S CURRENTLY LOGGED IN USER
-
-   if(ParseUser.getCurrentUser() != null)
-   {
-     Log.i("Signed In",ParseUser.getCurrentUser().getUsername());
-   }
-   else
-   {
-     Log.i("No luck","Not signed in");
-   }
-
-
- */
-
-/*
-
-// TO LOG SOMEONE OUT OF THE APP
-
-    ParseUser.logOut();
-
- */
-
-
-
-
-
+    logInTextView= findViewById(R.id.logInTextView);
+    logInTextView.setOnClickListener(this);
 
 
     ParseAnalytics.trackAppOpenedInBackground(getIntent());
   }
+
 
 }
